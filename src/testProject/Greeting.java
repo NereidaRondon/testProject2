661422e368b0d7cb69299c1b6250c9b48fe74dc0
nereida-promineo <nereida.promineo@gmail.com>
2023-05-12 09:41:27 -0400

    first commit

---
+++ src/testProject/Greeting.java
@@ -0,0 +1,18 @@
+package testProject;
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print("🌎 ");
+		}
+		
+		System.out.print("Hello World! ");
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print("🌎 ");
+		}
+	}
+
+}
